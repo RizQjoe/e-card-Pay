066d94649de6c379d1f4cda7de4511a521cf5aab
akbarsahata <akbarsahata@outlook.co.id>
2017-09-15 06:44:51 +0700

    front-end warung near done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -17,6 +18,12 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(session({
+  secret: 'i dont know',
+  saveUninitialized: true,
+  resave: true,
+  cookie: {}
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,68 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  let total = 0
+  if (req.session.cart) {
+    total = req.session.cart
+              .map(item => item.total)
+              .reduce((t,c) => parseInt(c)+t, 0);
+  } 
+  res.render('index', { inCart: (req.session.cart ? req.session.cart : []), totalInCart: total });
 });
 
-router.get('top-up', function(req, res, next) {
-  res.render('top-up')
+router.get('/top-up', function(req, res, next) {
+  res.render('top-up');
+});
+
+router.get('/add-customer', function(req, res, next) {
+  res.render('add-customer');
+});
+
+router.get('/checkout', function(req, res, next) {
+  req.session.destroy();
+  res.redirect('/');
+  // res.render('checkout', { items: {} })
+})
+
+router.get('/add-to-cart/:itemid/:itemname/:itemprice', function(req, res, next) {
+  console.log(req.session.cart);
+
+  let id = req.params.itemid,
+      name = req.params.itemname,
+      price = req.params.itemprice;
+
+  if (req.session.cart) {
+    let index = req.session.cart
+                  .map(item => item.name)
+                  .indexOf(name);
+
+    let found = index !== -1;
+
+    if (found) {
+      req.session.cart[index].total++
+    } else {
+      let obj = {};
+      obj.id = id;
+      obj.name = name;
+      obj.price = price;
+      obj.total = 1;
+
+      req.session.cart.push(obj);
+    }
+  } else {
+    req.session.cart = [];
+
+    let obj = {};
+    obj.id = id;
+    obj.name = name;
+    obj.price = price;
+    obj.total = 1;
+
+    req.session.cart.push(obj);
+  }
+
+  res.redirect('/');
+
 })
 
 module.exports = router;
